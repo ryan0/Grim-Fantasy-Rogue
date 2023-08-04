1ee761a9c3ead3a39fe2471c3199194c31e511a8
jcdentonmj12 <134112720+jcdentonmj12@users.noreply.github.com>
2023-08-04 00:12:12 -0400

    Art changes

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public float moveSpeed = 0.5f; // Speed at which the character moves
+    private bool isMoving;
+    private Vector3 targetPosition;
+    private TileData[,] tileDataMatrix; // Reference to your tile data matrix
+    public int mapWidth = 50;
+    public int mapHeight = 50;
+
+    private void Start()
+    {
+        tileDataMatrix = TileMapGenerator.tileDataMatrix;
+    }
+
+    private void Update()
+    {
+        if (isMoving)
+        {
+            float step = moveSpeed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, step);
+
+            if (Vector3.Distance(transform.position, targetPosition) < 0.001f)
+            {
+                isMoving = false;
+            }
+        }
+        else
+        {
+            Move();
+        }
+    }
+
+    private void Move()
+    {
+        int xDir = 0;
+        int yDir = 0;
+
+        if (Input.GetKeyDown(KeyCode.UpArrow)) yDir = 1;
+        if (Input.GetKeyDown(KeyCode.DownArrow)) yDir = -1;
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) xDir = -1;
+        if (Input.GetKeyDown(KeyCode.RightArrow)) xDir = 1;
+
+        if (xDir != 0 || yDir != 0)
+        {
+            int x = Mathf.FloorToInt(transform.position.x) + xDir;
+            int y = Mathf.FloorToInt(transform.position.y) + yDir;
+
+            if (x >= 0 && x < mapWidth && y >= 0 && y < mapHeight && tileDataMatrix[x, y].CanWalk)
+            {
+                targetPosition = new Vector3(x, y, transform.position.z);
+                isMoving = true;
+            }
+        }
+    }
+}
+
--- Assets/Scripts/TileData.cs
+++ Assets/Scripts/TileData.cs
@@ -15,11 +15,13 @@ public class TileData
     public int Type; // Tile type (1 for water, 2 for dirt, 3 for tree, etc.)
     public int Motes; // motes ranging from 1 to 100
     public int Height; // Height ranging from -100, to -2, to 2
+    public bool CanWalk;
 
-    public TileData(int type, int motes, int height)
+    public TileData(int type, int motes, int height, bool canWalk)
     {
         Type = type;
         Motes = motes;
         Height = height;
+        CanWalk = canWalk;
     }
 }
